7eb52bedd38727853b763170a36ff8e106fed60a
Charles Henrique de Paula Santos <charleshps.dev@gmail.com>
2025-10-05 14:31:52 -0300

    ciração do reaproveitamento de codigo do header com javascript

--- public/js/app.js
+++ public/js/app.js
@@ -14,3 +14,36 @@ function fecharMenuMobile(){
     menuMobile.classList.remove('menu-ativo');
 
 }
+
+function menuPagina(){
+    document.getElementById('cabecalho').innerHTML = '<nav id="menu">'+
+                '<div id="itens-desktop">'+
+                    '<a id="logo-site" href="index.html"><img id="logo" src="../img/logo_all_universo.png" alt="log al universo"></a>'+
+                    '<div id="lista-menu-desktop">'+
+                        '<ul id="menu-desktop">'+
+                            '<li class="itens-lista-desktop">'+
+                                '<a class="paginas" href="sobre.html">SOBRE</a>'+
+                            '</li>'+
+                            '<li class="itens-lista-desktop">'+
+                                '<a class="paginas" href="categorias.html">CATEGORIAS</a>'+
+                            '</li>'+
+                        '</ul>'+
+                    '</div>'+
+                '</div>'+
+                '<div id="itens-mobile">'+
+                    '<img onclick="abrirMenuMobile()" id="open-menu-lista" src="../img/icon_menu.png" alt="icone de menu">'+
+                    '<ul class="menu-mobile" id="id-menu-mobile" >'+
+                        '<li onclick="fecharMenuMobile()" id="close-menu-lista">'+
+                            '<img id="img-close-menu" src="../img/icon_x.png" alt="icone de menu">'+
+                        '</li>'+
+                        '<li class="itens-lista-mobile">'+
+                            '<a class="paginas" href="sobre.html">SOBRE123</a>'+
+                        '</li>'+
+                        '<li class="itens-lista-mobile">'+
+                            '<a class="paginas" href="categorias.html">CATEGORIAS</a>'+
+                        '</li>'+
+                    '</ul>'+
+                '</div>'+
+            '</nav>';
+
+}
