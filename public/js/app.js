c7de2674de68dc08505ed4cec2dbad56b010fea4
Charles Henrique de Paula Santos <charleshps.dev@gmail.com>
2025-10-05 18:14:00 -0300

    finalização do reaproveitamento de codigo das paginas

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,46 @@
 /*Dados a serem usados*/
 
-let conteudo = [
-    {"id": 1, "titulo": "Antes do Universo", "resumo": "Antes de entender como tudo começou, entenda como era antes! Viaje no universo de prótons, nêutrons, entre outras partículas!"},
-    {"id": 2, "titulo": "O Big Bang", "resumo": "Não foi uma explosão, mas o nascimento do tempo. Descubra o que realmente aconteceu no primeiro microssegundo e como a expansão do cosmos moldou tudo o que você vê. O início de tudo é mais estranho do que parece."},
-    {"id": 3, "titulo": "Buraco Negro", "resumo": "Eles são os devoradores silenciosos do cosmos. O que acontece com a matéria que cruza o ponto sem retorno? Desvende o enigma da gravidade extrema e dos segredos que a luz não pode revelar."},
-    {"id": 4, "titulo": "Via Láctea", "resumo": "Nossa casa é um caos organizado de 200 bilhões de estrelas, mas você conhece o gigante no centro? Explore a majestade da nossa galáxia e o buraco negro que comanda sua dança."},
-    {"id": 5, "titulo": "Estrelas", "resumo": "São as fornalhas que criaram todos os elementos do seu corpo. Como elas nascem, vivem e morrem de formas espetaculares? Entenda por que somos, literalmente, poeira estelar."}
+let resumo = [
+    {"id": 1, "titulo": "Antes do Universo", "imagem": "../img/img_antes_universo.png", "resumo": "Antes de entender como tudo começou, entenda como era antes! Viaje no universo de prótons, nêutrons, entre outras partículas!"},
+    {"id": 2, "titulo": "O Big Bang", "imagem": "../img/img_big_bang.png", "resumo": "Não foi uma explosão, mas o nascimento do tempo. Descubra o que realmente aconteceu no primeiro microssegundo e como a expansão do cosmos moldou tudo o que você vê. O início de tudo é mais estranho do que parece."},
+    {"id": 3, "titulo": "Buraco Negro", "imagem": "../img/img_buraco_negro.png", "resumo": "Eles são os devoradores silenciosos do cosmos. O que acontece com a matéria que cruza o ponto sem retorno? Desvende o enigma da gravidade extrema e dos segredos que a luz não pode revelar."},
+    {"id": 4, "titulo": "Via Láctea", "imagem": "../img/img_via_lactea.png", "resumo": "Nossa casa é um caos organizado de 200 bilhões de estrelas, mas você conhece o gigante no centro? Explore a majestade da nossa galáxia e o buraco negro que comanda sua dança."},
+    {"id": 5, "titulo": "Estrelas", "imagem": "../img/img_estrela.png", "resumo": "São as fornalhas que criaram todos os elementos do seu corpo. Como elas nascem, vivem e morrem de formas espetaculares? Entenda por que somos, literalmente, poeira estelar."}
 ]
 
+let conteudo = [
+    {"id": 1, "titulo": "Antes do Universo", "conteudo1": "", "conteudo2": "", "conteudo3": "", "conteudo4": "", "conteudo5": ""},
+    {"id": 2, "titulo": "O Big Bang", "conteudo1": "", "conteudo2": "", "conteudo3": "", "conteudo4": "", "conteudo5": ""},
+    {"id": 3, "titulo": "Buraco Negro", "conteudo1": "", "conteudo2": "", "conteudo3": "", "conteudo4": "", "conteudo5": ""},
+    {"id": 4, "titulo": "Via Láctea", "conteudo1": "", "conteudo2": "", "conteudo3": "", "conteudo4": "", "conteudo5": ""},
+    {"id": 5, "titulo": "Estrelas", "conteudo1": "", "conteudo2": "", "conteudo3": "", "conteudo4": "", "conteudo5": ""}
+]
+
+function carregarDadosIndex(){
+    let assuntos = document.getElementById('assuntos');
+    let dadosAssuntos = ''; 
+
+    for(let i = 0; i < resumo.length; i++){
+        let titulo = resumo[i]; 
+        let imagemCarregar = titulo.imagem;
+        let tituloCarregar = titulo.titulo;
+        let resumoCarregar = titulo.resumo;
+        dadosAssuntos +=  '<div class="itens-assuntos">'+
+                    '<img class="img-assuntos" src="'+ imagemCarregar+'" alt="imagem representativa do inicio do universo">'+
+                    '<h2 class="titulo-assuntos cor-branco">'+                       
+                        tituloCarregar +
+                    '</h2>'+
+                    '<p class="texto-assuntos cor-branco">'+
+                        resumoCarregar +
+                    '</p>'+
+                    '<button onclick="exibirConteudo()" class="botao-assuntos">'+
+                        'Ler mais'+
+                    '</button>'+
+                '</div>';   
+    }
+
+    assuntos.innerHTML = dadosAssuntos;
+}
 
 /*Função para abrir o menu mobile*/
 function abrirMenuMobile(){
@@ -26,8 +59,8 @@ function fecharMenuMobile(){
     menuMobile.classList.remove('menu-ativo');
 
 }
-/*Função para exibir o cabeçalho em todas as paginas e reutilizar o codigo*/
 
+/*Função para exibir o cabeçalho em todas as paginas e reutilizar o codigo*/
 function menuPagina(){
     document.getElementById('cabecalho').innerHTML = '<nav id="menu">'+
                 '<div id="itens-desktop">'+
